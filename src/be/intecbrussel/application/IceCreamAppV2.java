c8021b33e73ea819bbe4674106f94711e39fef02
Zuhur Ahmed <91530474+zuhurahmed@users.noreply.github.com>
2021-12-02 21:46:27 +0100

    Update IceCreamAppV2.java

--- src/be/intecbrussel/application/IceCreamAppV2.java
+++ src/be/intecbrussel/application/IceCreamAppV2.java
@@ -10,35 +10,28 @@ import be.intecbrussel.seller.*;
 public class IceCreamAppV2 {
         public static void main(String[] args)   {
            
-            PriceList priceList = new PriceList(2, 1.5, 2.5);
-            Stock stock = new Stock(4, 1, 3, 1);
-            IceCreamSeller iceCreamCar = new IceCreamCar(100, stock);
+            
+        new PriceList(2.0D, 1.5D, 2.5D);
+        Stock stock = new Stock(4, 1, 3, 1);
+        IceCreamSeller iceCreamCar = new IceCreamCar(100.0D, stock);
+        System.out.println("This is my IceCreamCar because\n of the covid19\nwe didn't sell a lot of ice cream but\n we hope the situation\n will become normal and by the way\n our ice cream are delicious and\nthank you for ordering from us");
+        System.out.println("  you can order some IceCream in here: ");
+        System.out.println("ValueOfIceCream:Profit of the IceCreamCar: " + iceCreamCar.getProfit() + " €");
+        System.out.println("value of the Ice rockets/magnum/balls in the stock: ");
 
-
-
-         
-            Eatable[] orders = new Eatable[]{};
-            try {
-                Eatable[] orders1 =
-                        {iceCreamCar.orderCone( new Cone.Flavor[]{Cone.Flavor.BANANA, Cone.Flavor.CHOCOLATE, Cone.Flavor.MOKKA}),
-                                iceCreamCar.orderMagnum(Magnum.MagnumType.ALPINENUTS),
-                                iceCreamCar.orderMagnum(Magnum.MagnumType.MILKCHOCOLATE), iceCreamSeller.orderIceRocket()};
-                orders =orders1;
-            }catch (Exception e){
-                System.out.println(e.getMessage());
-            }
-
-
-            System.out.println("ValueOfIceCream");
-            System.out.println("Profit of the IceCreamCar: " + iceCreamCar.getProfit() + " €");
-            System.out.println("Value of the balls in the stock: " + iceCreamCar);
-            System.out.println("Value of the cones in the stock: " + iceCreamCar.orderCone(Cone.Flavor.values()));
-            System.out.println("value of the Ice rockets in the stock: " + iceCreamCar.orderIceRocket());
-            System.out.println("value of the Magnums in the stock: " + iceCreamCar.orderMagnum(Magnum.MagnumType.MILKCHOCOLATE));
+        try {
+            Eatable[] orders = new Eatable[]{iceCreamCar.orderCone(new Flavor[]{Flavor.BANANA, Flavor.CHOCOLATE, Flavor.MOKKA}), iceCreamCar.orderMagnum(MagnumType.ALPINENUTS), iceCreamCar.orderMagnum(MagnumType.MILKCHOCOLATE), iceCreamCar.orderIceRocket()};
+            System.out.println(Arrays.toString(orders));
+        } catch (Exception var5) {
+            System.out.println(var5.getMessage());
         }
 
-
     }
+                
+                
+                
+                
+      
 
 
 
